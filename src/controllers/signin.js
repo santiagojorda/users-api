e34531a493c7defd2f2a2b283c4ec813807d491a
santiagojorda <sjorda@fi.uba.ar>
2023-03-10 01:30:55 -0300

    borrando comentarios

--- src/controllers/signin.js
+++ src/controllers/signin.js
@@ -1,6 +1,5 @@
 const User = require('../models/user')
 
-
 const signinCtrl = (req, res) => {
 
     const {username, password, email} = req.body
@@ -17,19 +16,6 @@ const signinCtrl = (req, res) => {
             res.status(201).json(savedUser)
         })
         .catch((err) => console.error(err))
-
-    // newUser.save((err) => {
-    //     if(err){
-    //         console.log('there was an saving user error')
-    //         console.log(err)
-    //         res.status(401).json({message: err})
-    //     }
-    //     else {
-    //         console.log('user created successfully')
-    //     }
-        
-    // })
-    // res.status(201).json({username: username})
     
 }   
 
