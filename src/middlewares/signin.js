a7e2cd9d9626fd70fa1eca5d20199ed4b1c1a659
santiagojorda <sjorda@fi.uba.ar>
2023-03-10 00:37:29 -0300

    test: signin creacion, falta de campos y usuario existente

--- src/middlewares/signin.js
+++ src/middlewares/signin.js
@@ -1,7 +1,43 @@
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
-const signinMw = (req, res) => {
-    res.json({ message: 'hola' })
-    res.end()
+const userRequiredFieldsAreCompleted = (user) => {
+    return user.username && user.password && user.email
 }
 
-module.exports = signinMw
+const userFieldsAreCorrect = (user) => {
+    return true
+}
+
+const thisUserAlredyExist = async (user) => {
+    const userFinded = await User
+        .findOne({username: user.username})
+        .then((userFinded) => userFinded)
+        
+    if(!userFinded)
+        return false
+    return true
+}
+
+const signinMiddleware = async (req, res, next) => {
+
+    const {username, password, email} = req.body
+
+    const newUser = {
+        username: username,
+        password: password,
+        email: email
+    }
+
+    if(!userRequiredFieldsAreCompleted(newUser))
+        res.status(400).json({error: 'username, password and email are required'})
+    else if(!userFieldsAreCorrect(newUser))
+        res.status(400).json({error: 'username, password or email has an incorrect format'})
+    else if(await thisUserAlredyExist(newUser))
+        res.status(400).json({error: 'this user already exist'})
+    else
+        next()
+    
+}
+
+module.exports = signinMiddleware
